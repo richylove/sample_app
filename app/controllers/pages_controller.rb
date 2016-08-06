10692dd5288f1e94d17274e806b61705a8ee2ae6
richylove <richylove18@yahoo.fr>
2016-08-06 03:13:14 +0200

    Removed default Rails page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,8 @@ class PagesController < ApplicationController
   def about
     @titre = "À Propos"
   end
+
+  def help
+    @title = "Aide"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/home' => 'pages#home'
-  get 'pages/contact' => 'pages#contact'
-  get 'pages/about' => "pages#about"
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
